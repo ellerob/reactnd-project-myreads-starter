e735bacac7c5c937f6a4a794ef867ddcd84630ee
Eleanor <eleanorrobinson@deloitte.co.uk>
2018-03-20 16:18:24 +0000

    renders three book shelves correctly named

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import BookShelf from './BookShelf'
 
 class BooksApp extends React.Component {
   render() {
+    const shelves = [
+      { id: 'currentlyReading', name: 'Currently Reading' },
+      { id: 'wantToRead', name: 'Want To Read' },
+      { id: 'read', name: 'Read' }
+    ]
     return (
-      <BookShelf/>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+          {shelves.map((shelf) => (
+              <li key={shelf.id} className="bookshelf">
+            <BookShelf 
+              shelf={shelf}
+            />
+            </li>
+          ))}
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,8 +5,8 @@ import Book from './Book'
 class BookShelf extends React.Component {
   render() {
     return (
-      <div className="bookshelf">
-      <h2 className="bookshelf-title">Want to Read</h2>
+      <div>
+      <h2 className="bookshelf-title">{this.props.shelf.name}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           <li>
